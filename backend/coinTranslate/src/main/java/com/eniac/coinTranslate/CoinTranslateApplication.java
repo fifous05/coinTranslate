afccb635fb606f1d3fa2e700b3049890d979388d
vitor <vitorlourenco2005@gmail.com>
2026-04-17 19:30:01 -0300

    update backend

--- backend/coinTranslate/src/main/java/com/eniac/coinTranslate/CoinTranslateApplication.java
+++ backend/coinTranslate/src/main/java/com/eniac/coinTranslate/CoinTranslateApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.eniac.coinTranslate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CoinTranslateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CoinTranslateApplication.class, args);
 	}
 
 }
