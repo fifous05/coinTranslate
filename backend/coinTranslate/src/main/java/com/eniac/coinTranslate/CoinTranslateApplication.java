1ce2c6a8db6e9416c6f600ee3ebbd45e3cc868a2
vitor <vitorlourenco2005@gmail.com>
2026-04-17 19:39:22 -0300

    new class

--- backend/coinTranslate/src/main/java/com/eniac/coinTranslate/CoinTranslateApplication.java
+++ backend/coinTranslate/src/main/java/com/eniac/coinTranslate/CoinTranslateApplication.java
@@ -1,34 +1,13 @@
-package com.eniac.coinTranslate;// mantenha o seu package!
+package com.eniac.coinTranslate;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "*")
+@SpringBootApplication
+public class CoinTranslateApplication {
 
-public class ConversorController {
-
-	@GetMapping("/converter")
-	public double converter(
-			@RequestParam double valor,
-			@RequestParam String moeda) {
-
-		double cotacao = 0;
-
-		switch (moeda.toLowerCase()) {
-			case "dolar":
-				cotacao = 5.0;
-				break;
-			case "euro":
-				cotacao = 5.5;
-				break;
-			case "libra":
-				cotacao = 6.3;
-				break;
-			default:
-				return 0;
-		}
-
-		return valor / cotacao;
+	public static void main(String[] args) {
+		SpringApplication.run(CoinTranslateApplication.class, args);
 	}
+
 }
