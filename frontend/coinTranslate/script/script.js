67dcc831e2928341e4f2e246a37a92fa1a95a34d
Vitor <vitorlourenco2005@gmail.com>
2026-04-23 21:39:05 -0300

    Checkpoint- frontend

--- frontend/coinTranslate/script/script.js
+++ frontend/coinTranslate/script/script.js
@@ -1,15 +1,12 @@
 function converter() {
     let valor = document.getElementById("valor").value;
-    let moeda = document.getElementById("moeda").value;
+    let origem = document.getElementById("origem").value;
+    let destino = document.getElementById("destino").value;
 
-    fetch(`http://localhost:8080/api/converter?valor=${valor}&moeda=${moeda}`)
+    fetch(`http://localhost:8080/api/converter?valor=${valor}&origem=${origem}&destino=${destino}`)
         .then(res => res.json())
         .then(data => {
             document.getElementById("resultado").innerText =
-    `R$ ${data.valor_original} → ${data.resultado} (${data.moeda})`;
-        })
-        .catch(err => {
-            console.error(err);
-            alert("Erro ao conectar com o backend");
+                `${data.valor_original} (${data.origem}) → ${data.resultado} (${data.destino})`;
         });
-    }
+}
